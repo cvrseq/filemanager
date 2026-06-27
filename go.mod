f16a827749508b2b6eb623378c2bf24f90d9ab29
a <agasha@Aleksandrs-MacBook-Air.local>
2026-06-27 12:48:46 -0700

    desc components for filemanager

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cvrseq/filemanager
+
+go 1.25.5
---
+++ internal/file.go
@@ -0,0 +1,34 @@
+package file
+
+import (
+	"time"
+)
+type Object struct { 
+	file *File
+	dir *Directory
+}
+
+type Directory struct { 
+	name string
+	writes string
+	filenames string
+}
+
+type Metadata struct { 
+	filename string
+	size uint64
+	kind *Object
+}
+
+type File struct { 
+	name string
+	size uint64
+	permissions string
+	owner string
+	created_at time.Time
+	metadata *Metadata
+}
+
+func Create(object string) bool {
+	return true 
+}
